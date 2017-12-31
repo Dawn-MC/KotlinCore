1cf5716f3e16b3cc99d24efc2ce8474257111e22
Johnfg10 <johnfg2610@gmail.com>
2017-12-31 12:03:25 +0000

    fixed creation error(now registers as a plugin and can be required by other plugins)

---
+++ src/main/kotlin/com/dawndevelop/KotlinCore.kt
@@ -0,0 +1,7 @@
+package com.dawndevelop
+
+import org.spongepowered.api.plugin.Plugin
+
+@Plugin(id = "kotlincore", name = "Kotlin core", description = "Includes all of kotlin's library's required for a Kotlin based plugin", version = "1.2.10")
+class KotlinCore {
+}
